591b7b281afc799b14ea1b0f1ce97c3ec349b065
Luis Angel Torres <latorr13@asu.edu>
2026-03-18 23:30:48 -0700

    Add player turn prompts for both players

---
+++ src/TicTacToe.java
@@ -0,0 +1,7 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to TicTacToe!");
+        System.out.println("Player 1, make your move!");
+        System.out.println("Player 2, make your move!");
+    }
+}
