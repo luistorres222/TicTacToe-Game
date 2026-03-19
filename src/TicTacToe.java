64688a5f57c978adc5bd53169f4965c1b1ce7f54
Luis Angel Torres <latorr13@asu.edu>
2026-03-18 23:44:26 -0700

    Add game starting message for better user feedback

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,5 +3,6 @@ public class TicTacToe {
         System.out.println("Welcome to TicTacToe!");
         System.out.println("Player 1, make your move!");
         System.out.println("Player 2, make your move!");
+        System.out.println("Game starting...");
     }
 }
